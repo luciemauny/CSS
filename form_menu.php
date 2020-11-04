6d21f11e701a6cb0c06c62c101be58e7fac0d116
luciemauny <73611095+luciemauny@users.noreply.github.com>
2020-11-04 09:54:28 +0100

    Create form_menu.php

---
+++ form_menu.php
@@ -0,0 +1,138 @@
+<html lang="fr">
+<head>
+    <title>Critique_jeux_plateau</title>
+</head>
+<style>
+    input[type=submit]{
+        background-color: black;
+        border: none;
+        color: white;
+        padding: 8px 16px;
+        text-decoration: none;
+        margin: 4px 2px;
+        cursor: pointer;
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+    }
+    body {
+        background-image: url('https://images.pexels.com/photos/1323712/pexels-photo-1323712.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260');
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: 100% 100%;
+        border: 2px solid black;
+    }
+    div.transbox {
+        width: 250px;
+        height: 200px;
+        border-radius: 50%;
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+        margin: auto;
+    }
+
+    div.transbox p {
+        margin: auto;
+        font-weight: bold;
+        color: #000000;
+        text-align: center;
+    }
+    h1 {
+        font-size: 40px;
+        text-align: center;
+    }
+    div.texte{
+        text-align: center;
+        font-size : 20px
+    }
+</style>
+</br>
+<h1> Bonjour <?php
+    echo $_SESSION["pseudo"]; ?> !</h1></br>
+<body>
+<div class="texte">
+    Que souhaitez vous faire ?
+</div></br>
+<form method="post" action="page_accueil.php">
+
+    <table cellspacing="50px" style="margin: auto">
+
+        <tr>
+            <th>
+                <div class="transbox" style="background-color: rgba(131, 0, 132,10)">
+                    <label loupe >
+                        </br>
+                            <img src="https://img.icons8.com/android/96/000000/search.png"/>
+                        </br></br>
+                            <input type="submit" name="recherche_jeu" value="Rechercher des jeux">
+                    </label>
+                </div>
+            </th>
+
+
+            <th>
+                <div class="transbox" style="background-color: rgba(0, 187, 254, 1)">
+                    <label plus>
+                        </br>
+                            <img src="https://img.icons8.com/android/96/000000/plus.png"/>
+                        </br></br>
+                            <input type="submit" name="ajout_jeu" value="Ajouter des jeux">
+                    </label>
+                </div>
+            </th>
+
+
+            <th>
+                <div class="transbox" style="background-color: rgba(0, 182, 0, 1)">
+                    <label pion>
+                        </br>
+                            <img src="https://img.icons8.com/fluent-systems-filled/96/000000/rook.png"/>
+                        </br></br>
+                            <input type="submit" name="decourvrir_jeux" value="Décourvir des jeux">
+                    </label>
+                </div>
+            </th>
+</form>
+        </tr>
+
+        <tr>
+            <th>
+                <div class="transbox" style="background-color: rgba(255, 249, 0, 1)">
+                    <label voir ses amis>
+                        <form method="post" action="affichage_amis.php">
+                            </br>
+                                <img src="https://img.icons8.com/material-sharp/96/000000/find-user-male.png"/>
+                            </br></br>
+                                <input type="submit" name="amis" value="Voir mes amis">
+                        </form>
+                    </label>
+                </div>
+            </th>
+
+            <th>
+                <div class="transbox" style="background-color: rgba(255, 175, 0, 1)">
+                    <label ajouter des amis>
+                        <form method="post" action="page_accueil.php">
+                            </br>
+                                <img src="https://img.icons8.com/material-rounded/96/000000/add-user-male.png"/>
+                            </br></br>
+                                 <input type="submit" name="ajout_amis" value="Ajouter des amis">
+                        </form>
+                    </label>
+                </div>
+            </th>
+
+            <th>
+                <div class="transbox" style="background-color: rgba(255, 74, 0, 1)">
+                    <label voir les notes de ses amis>
+                        <form method="post" action="notes_amis.php">
+                            </br>
+                                <img src="https://img.icons8.com/fluent-systems-filled/96/000000/speaker-notes.png"/>
+                            </br></br>
+                                <input type="submit" name="notes" value="Voir les notes de mes amis">
+                        </form>
+                    </label>
+                </div>
+            </th>
+        </tr>
+    </table>
+
+</body>
+</html>
