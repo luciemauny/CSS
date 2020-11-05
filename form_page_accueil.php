483b8ef6910d5caa6085c29b8a5fb3ee354fb300
jmtrn <martinjustinemartinjustine@gmail.com>
2020-11-05 18:47:44 +0100

    css à part

--- form_page_accueil.php
+++ form_page_accueil.php
@@ -6,47 +6,8 @@ session_destroy();
 <html lang="fr">
 <head>
     <title>Critique_jeux_plateau</title>
+    <link rel="stylesheet" type="text/css" href="page_accueil.css" media="all"/>
 </head>
-
-<style>
-    input[type=submit]{
-        background-color: black;
-        border: none;
-        color: white;
-        padding: 8px 16px;
-        text-decoration: none;
-        margin: 4px 2px;
-        cursor: pointer;
-        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
-    }
-    body {
-        background-image: url('https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-board-games-2018-1538589179.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*');
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-        background-size: 100% 100%;
-        border: 2px solid black;
-    }
-
-    div.transbox {
-        margin: 5px;
-        background-color: #ffffff;
-        border: 1px solid black;
-        opacity: 0.6;
-
-    }
-
-    div.transbox p {
-        margin: 5%;
-        font-weight: bold;
-        color: #000000;
-        text-align: center;
-    }
-    div.titre{
-        font-size: 100px;
-    }
-
-</style>
-
 <h1>
     <form method="post" action="page_accueil.php">
         <table cellspacing="0" style="margin-left: 900px;width:10%;">
