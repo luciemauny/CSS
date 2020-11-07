018341a799c8bdc06a3df9960bac349265777b73
luciemauny <73611095+luciemauny@users.noreply.github.com>
2020-11-07 10:45:27 +0100

    Update connecter.php

--- connecter.php
+++ connecter.php
@@ -11,48 +11,48 @@ $data=$req->fetch();
 
 if(empty($data)){
 
-?>
+    ?>
 
     <html lang="fr">
-            <head>
-                <title>Critique_jeux_plateau</title>
-                <link rel="stylesheet" type="text/css" href="confirmation.css" media="all"/>
-            </head>
-            <body>
-            <form method="post" action="page_accueil.php";>
-                <div class="jeu">
+    <head>
+        <title>Critique_jeux_plateau</title>
+        <link rel="stylesheet" type="text/css" href="confirmation.css" media="all"/>
+    </head>
+    <body>
+    <form method="post" action="page_accueil.php";>
+        <div class="jeu">
 
-                    <sub><img src="https://img.icons8.com/windows/96/000000/queen.png" width="40" height="40"/></sub>
-                    OOPS !
-                    <sub><img src="https://img.icons8.com/windows/96/000000/queen.png" width="40" height="40"/></sub>
-                </div>
-                <div class="animation1">
+            <sub><img src="https://img.icons8.com/windows/96/000000/queen.png" width="40" height="40"/></sub>
+            OOPS !
+            <sub><img src="https://img.icons8.com/windows/96/000000/queen.png" width="40" height="40"/></sub>
+        </div>
+        <div class="animation1">
 
-                </div>
-                </br>
-                <div class="texte">
-                    Le mot de passe ou l'identifiant n'existe pas ! Cliquez ici pour réessayer:
-                </div>
+        </div>
+        </br>
+        <div class="texte">
+            Le mot de passe ou l'identifiant n'existe pas ! Cliquez ici pour réessayer:
+        </div>
 
-                </br> </br>
-                <div class="animation2"></div>
-                <div class="animation4"></div>
-                <div class="animation3"></div>
+        </br> </br>
+        <div class="animation2"></div>
+        <div class="animation4"></div>
+        <div class="animation3"></div>
 
-                <div class="box">
-                    <label>
-                        <p><input type="submit" name="connecter" value="Réessayer"></p>
-                    </label>
-                </div>
+        <div class="box">
+            <label>
+                <p><input type="submit" name="connecter" value="Réessayer"></p>
+            </label>
+        </div>
 
-            </form>
-            </body>
-            </html>
+    </form>
+    </body>
+    </html>
 
-            <?php
+    <?php
 }else{
     $_SESSION["id"]=$data['id'];
     $_SESSION["pseudo"]=$data['pseudo'];
-    include('form_menu.html');
+    include('form_menu.php');
 }
-        ?>
+?>
