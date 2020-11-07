2875d010f1d86fab74f92bb4bb3d979b7ef6b0dd
luciemauny <73611095+luciemauny@users.noreply.github.com>
2020-11-07 10:50:51 +0100

    Update notes_amis.php

--- notes_amis.php
+++ notes_amis.php
@@ -3,9 +3,13 @@
     <title>Critique_jeux_plateau</title>
     <link rel="stylesheet" type="text/css" href="tableaux.css" media="all"/>
 </head>
-
+<style>
+    input[type=submit]:hover {
+        background-color: rgba(255, 74, 0, 1);
+    }
+</style>
 <body>
-<div class="jeu">
+<div class="jeu" style="text-decoration: underline rgba(255, 74, 0, 1)">
     <sub><img src="https://img.icons8.com/windows/96/000000/queen.png" width="40" height="40"/></sub>
     Notes données par mes amis
     <sub><img src="https://img.icons8.com/windows/96/000000/queen.png" width="40" height="40"/></sub>
@@ -18,35 +22,42 @@
     </tr>
 
 
-<?php
-session_start();
+    <?php
+    session_start();
 
 
-$bdd = new PDO("mysql:host=localhost;dbname=critique_jeux_plateau;charset=utf8", "root", "");
-$req = $bdd->prepare("SELECT utilisateur.pseudo,note.note,jeu.nom_jeu FROM utilisateur 
+    $bdd = new PDO("mysql:host=localhost;dbname=critique_jeux_plateau;charset=utf8", "root", "");
+    $req = $bdd->prepare("SELECT utilisateur.pseudo,note.note,jeu.nom_jeu FROM utilisateur 
 INNER JOIN amis ON utilisateur.id=amis.id_amis INNER JOIN note ON note.id_utilisateur=amis.id_amis 
 INNER JOIN jeu ON note.id_jeu=jeu.id_jeu WHERE amis.id_utilisateur=?");
-$req->execute([$_SESSION['id']]);
-$i=0;
-while ($data = $req->fetch()) {
-    if ($i % 2 == 0) {
-        echo '<tr style="background-color: #cccccc">';
+    $req->execute([$_SESSION['id']]);
+    $i=0;
+    while ($data = $req->fetch()) {
+        if ($i % 2 == 0) {
+            echo '<tr style="background-color: #cccccc">';
             echo '<td>';echo $data["pseudo"]; echo '</td>';
             echo '<td>';echo $data["nom_jeu"];echo '</td>';
             echo '<td>'; echo $data["note"];echo '</td>';
             echo'</tr>';
 
-}
-    else{
-        echo '<tr>';
+        }
+        else{
+            echo '<tr>';
             echo '<td>';echo $data["pseudo"]; echo '</td>';
             echo '<td>';echo $data["nom_jeu"];echo '</td>';
             echo '<td>'; echo $data["note"];echo '</td>';
             echo'</tr>';
+        }
+        $i++;
     }
-    $i++;
-}
-?>
+    ?>
+</table>
+    <form method="post" action="page_accueil.php">
 
-<body/>
-<html/>
+        </br></br></br>
+        <input type="submit" name="menu" value="MENU">
+
+    </form>
+
+    <body/>
+    <html/>
