b323c9afe1e426d0783c07a286d7b8d2694ef131
luciemauny <73611095+luciemauny@users.noreply.github.com>
2020-11-07 10:40:45 +0100

    Update affichage_amis.php

--- affichage_amis.php
+++ affichage_amis.php
@@ -3,11 +3,17 @@
     <title>Critique_jeux_plateau</title>
     <link rel="stylesheet" type="text/css" href="formulaire.css" media="all"/>
 </head>
+<style>
+    input[type=submit].amis {
+        background-color: #cccccc;
+    }
+    input[type=submit]:hover {
+        background-color: gold;
+    }
+</style>
 <body>
 
-
-<body>
-<div class="jeu">
+<div class="jeu" style="text-decoration: underline rgba(255, 249, 0, 1)">
 
     <sub><img src="https://img.icons8.com/windows/96/000000/queen.png" width="40" height="40"/></sub>
     Mes amis
@@ -25,53 +31,53 @@
 
         </br></br>
 
-<?php
-
-session_start();
-
-$bdd = new PDO("mysql:host=localhost;dbname=critique_jeux_plateau;charset=utf8", "root", "");
-
-
-$req = $bdd->prepare("SELECT id_amis  FROM amis WHERE id_utilisateur=?");
-$req->execute([$_SESSION['id']]);
-
-$i=0;
-while ($data = $req->fetch()) {
-    $reque = $bdd->prepare("SELECT utilisateur.pseudo FROM utilisateur INNER JOIN amis ON utilisateur.id=amis.id_amis
-    WHERE amis.id_amis=?");
-    $reque->execute([$data["id_amis"]]);
-    $datapseudo=$reque->fetch();
-    $requete = $bdd->prepare("SELECT utilisateur.id FROM utilisateur INNER JOIN amis ON utilisateur.id=amis.id_amis WHERE amis.id_utilisateur=? AND amis.id_amis=?");
-    $requete->execute([$data["id_amis"], $_SESSION['id']]);
-    $datamis = $requete->fetch();
-    if (empty($datamis['id'])){
-    ?><?php echo $datapseudo["pseudo"];?>
         <?php
-    }else{
-        echo '<span>';
-         echo $datapseudo["pseudo"]; echo'</span>'
-            ?> <?php
-    }
-    echo'</br>';
-    $i++;
-    ?>
-    <form method="post" action="gestion_amis.php">
-        <input type="hidden" name="id_amis" value="<?php echo $data["id_amis"] ?>">
-       <input type="submit" name="supprimer" value="supprimer cet ami">
-    </form>
 
-<?php
-}
-?>
+        session_start();
+
+        $bdd = new PDO("mysql:host=localhost;dbname=critique_jeux_plateau;charset=utf8", "root", "");
+
+
+        $req = $bdd->prepare("SELECT id_amis  FROM amis WHERE id_utilisateur=?");
+        $req->execute([$_SESSION['id']]);
+
+        $i=0;
+        while ($data = $req->fetch()) {
+            $reque = $bdd->prepare("SELECT utilisateur.pseudo FROM utilisateur INNER JOIN amis ON utilisateur.id=amis.id_amis
+    WHERE amis.id_amis=?");
+            $reque->execute([$data["id_amis"]]);
+            $datapseudo=$reque->fetch();
+            $requete = $bdd->prepare("SELECT utilisateur.id FROM utilisateur INNER JOIN amis ON utilisateur.id=amis.id_amis WHERE amis.id_utilisateur=? AND amis.id_amis=?");
+            $requete->execute([$data["id_amis"], $_SESSION['id']]);
+            $datamis = $requete->fetch();
+            if (empty($datamis['id'])){
+                ?><?php echo $datapseudo["pseudo"];?>
+                <?php
+            }else{
+                echo '<span>';
+                echo $datapseudo["pseudo"]; echo'</span>'
+                ?> <?php
+            }
+            echo'</br>';
+            $i++;
+            ?>
+            <form method="post" action="gestion_amis.php">
+                <input type="hidden" name="id_amis" value="<?php echo $data["id_amis"] ?>">
+                <input class="amis" type="submit" name="supprimer" value="supprimer cet ami">
+            </form>
+
+            <?php
+        }
+        ?>
 
     </div>
 
     <br/>
 
     <div class="texte">Les utilisateurs écrits en jaune sont ceux qui vous suivent aussi !
-    <form method="post" action="page_accueil.php">
-        <input type="submit" name="menu" value="MENU">
-    </form>
+        <form method="post" action="page_accueil.php">
+            <input type="submit" name="menu" value="MENU">
+        </form>
     </div>
 </div>
 </body>
