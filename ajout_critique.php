a464bd4d36a7a8017821f53812664a83787c6e3e
jmtrn <martinjustinemartinjustine@gmail.com>
2020-11-07 18:18:39 +0100

    commentaires ajout critique

--- ajout_critique.php
+++ ajout_critique.php
@@ -4,20 +4,23 @@ session_start();
 $bio=$_POST['bio'];
 $note=$_POST['note'];
 
-echo $note;
-
 $bdd = new PDO("mysql:host=localhost;dbname=critique_jeux_plateau;charset=utf8", "root", "");
+
+//récupère l'id du jeu pour lequel l'utilisateur a renseigné une critique et/ou note
 $req = $bdd->prepare("SELECT jeu.id_jeu FROM jeu WHERE jeu.nom_jeu=?");
 $req->execute([$_SESSION['nom_jeu']]);
 $data=$req->fetch();
 
+//récupère l'id de l'utilisateur connecté
 $req = $bdd->prepare("SELECT id FROM utilisateur WHERE pseudo=?");
 $req->execute([$_SESSION['pseudo']]);
 $data_utilisateur=$req->fetch();
 
+//insert le texte de la critique dans la table critique avec les informations récupérées ci-dessus
 $req = $bdd->prepare("INSERT INTO critique(bio, id_jeu, id_utilisateur) VALUES(?,?,?)");
 $req->execute([$bio,$data['id_jeu'],$data_utilisateur['id']]);
 
+//insert la note dans la table note avec les informations récupérées ci-dessus
 $req = $bdd->prepare("INSERT INTO note(note, id_jeu, id_utilisateur) VALUES(?,?,?)");
 $req->execute([$note,$data['id_jeu'],$data_utilisateur['id']]);
 ?>
